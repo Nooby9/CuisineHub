9f66f5ea3cefda26e630c0a00b13a44894a55d23
Nooby9 <1330762153@qq.com>
2024-07-31 18:06:28 -0700

    Initial Setup for the project in Expo

---
+++ Firebase/firebaseSetup.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3uVQUMOR_3z7nXB5b23vdO1ryynIS5kQ",
+  authDomain: "groupproject-81f8f.firebaseapp.com",
+  projectId: "groupproject-81f8f",
+  storageBucket: "groupproject-81f8f.appspot.com",
+  messagingSenderId: "1012058673691",
+  appId: "1:1012058673691:web:c9bebeacde1c7062241ebe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
