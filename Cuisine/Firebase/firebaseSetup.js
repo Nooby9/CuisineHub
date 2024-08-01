f6232d6d13ae2a9818120121fe5b923f13c2bf22
Nooby9 <1330762153@qq.com>
2024-07-31 18:56:34 -0700

    Set up firebase connection.

--- Cuisine/Firebase/firebaseSetup.js
+++ Cuisine/Firebase/firebaseSetup.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } from '@env';
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD3uVQUMOR_3z7nXB5b23vdO1ryynIS5kQ",
-  authDomain: "groupproject-81f8f.firebaseapp.com",
-  projectId: "groupproject-81f8f",
-  storageBucket: "groupproject-81f8f.appspot.com",
-  messagingSenderId: "1012058673691",
-  appId: "1:1012058673691:web:c9bebeacde1c7062241ebe"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
---
+++ Cuisine/Firebase/firestoreHelper.js
@@ -0,0 +1,12 @@
+import {addDoc, deleteDoc, updateDoc, collection, getDocs, doc} from 'firebase/firestore';
+import {database} from './firebaseSetup';
+
+export async function writeToDB(data, collectionName) {
+    try{
+        const docID = await addDoc(collection(database, collectionName), data);
+        console.log("Document written with ID: ", docID.id);
+    }
+    catch(e) {
+        console.error("Write to db: ", e);
+    }
+}
