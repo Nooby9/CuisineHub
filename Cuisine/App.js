48cc02539fe7b635ab780d5224a17bf217461e21
BLACK-POPCORN <wang.huiji@northeastern.edu>
2024-08-02 14:58:51 -0700

    Add tab screen HomeScreen; add post screen

--- Cuisine/App.js
+++ Cuisine/App.js
@@ -1,13 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import { writeToDB } from './Firebase/firestoreHelper';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { screenOptions } from './style';
+import HomeScreen from './screens/HomeScreen';
+import PostScreen from './screens/PostScreen';
 
+// Create navigators
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// Tab navigator component
+function Tabs() {
+  return (
+    <Tab.Navigator screenOptions={({ route }) => ({
+      ...screenOptions,
+      contentStyle: {
+        backgroundColor: "white",
+      },
+    })}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ tabBarIcon:({ color, size }) => ( <AntDesign name="home" size={24} color="black" />) }}
+      />
+
+    </Tab.Navigator>
+  );
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator screenOptions={({ route }) => ({
+        ...screenOptions,
+        contentStyle: {
+          backgroundColor: "white",
+        },
+      })}>
+          <Stack.Screen name="Home" component={Tabs} options={{ headerShown: false }} />
+          <Stack.Screen name="Post" component={PostScreen} options={{ title: 'Post Details' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
@@ -17,5 +54,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+ },
 });
