f5b5982b86a453e4fecd266f530ad5d594effdd1
Nooby9 <1330762153@qq.com>
2024-08-05 19:09:25 -0700

    Added update database function

--- Cuisine/Firebase/firestoreHelper.js
+++ Cuisine/Firebase/firestoreHelper.js
@@ -11,3 +11,13 @@ export async function writeToDB(data, collectionName) {
         console.error("Write to db: ", e);
     }
 }
+
+export async function updateDB(id, data, collectionName) {
+    try{
+        await updateDoc(doc(database, collectionName, id), data);
+    }
+    catch(e) {
+        console.error("Update db: ", e);
+    }
+}
+
